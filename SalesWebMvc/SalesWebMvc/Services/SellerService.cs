74440b61c9643732cb6162b231f3c226b28ab129
thiago jose da silva <thiago.ads.silva@gmail.com>
2023-06-03 06:29:09 -0300

    Organização

--- SalesWebMvc/SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/SalesWebMvc/Services/SellerService.cs
@@ -44,7 +44,7 @@ namespace SalesWebMvc.Services
             }
             catch (DbUpdateException e)
             {
-               // throw new IntegrityException("Can't delete seller because he/she has sales");
+               throw new IntegrityException("Can't delete seller because he/she has sales");
             }
         }
 
